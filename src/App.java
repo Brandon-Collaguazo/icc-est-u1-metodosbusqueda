516cf195129a8e523872770c73abe4c73692717e
Brandon <bcollaguazos@est.ups.edu.ec>
2025-04-15 07:56:25 -0500

    Implementacion del método

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
+import Controllers.metodosBusqueda;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        metodosBusqueda mb = new metodosBusqueda();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el elemento para buscar en el arreglo: ");
+        int d = sc.nextInt();
+        if (resultado != -1) {
+            System.out.println("Dato encontrado");
+        } else {
+            System.out.println("Dato no encontrado");
+        }
+        sc.close();
     }
 }
--- src/Controllers/metodosBusqueda.java
+++ src/Controllers/metodosBusqueda.java
@@ -2,5 +2,12 @@ package Controllers;
 
 public class metodosBusqueda {
     int [] arreglo = {3, 5, 1, 0, 8, 9, 10};
-    
+    public int busquedaLineal(int dato) {
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] == dato) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
